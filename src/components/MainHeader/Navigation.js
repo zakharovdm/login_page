4f7eb80191dd8858d8814a0b82957f1c188c5417
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-10-03 23:24:03 +0300

    feat: add styling for Navigation component

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -4,21 +4,23 @@ import Button from "../UI/Button/Button";
 const Navigation = (props) => {
   return (
     <nav className={classes.nav}>
-      {props.isLoggedIn && (
-        <li>
-          <a href="/">Users</a>
-        </li>
-      )}
-      {props.isLoggedIn && (
-        <li>
-          <a href="/">Admin</a>
-        </li>
-      )}
-      {props.isLoggedIn && (
-        <li>
-          <Button onClick={props.isLogout}>Logout</Button>
-        </li>
-      )}
+      <ul>
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <Button onClick={props.isLogout}>Logout</Button>
+          </li>
+        )}
+      </ul>
     </nav>
   )
 };
