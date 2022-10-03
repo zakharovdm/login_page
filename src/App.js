2f492947a2075f3b102ad9ed7075c8b623976b13
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-10-03 22:42:27 +0300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { useState, Fragment } from "react";
 
 import Login from "./components/Login/Login";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const logginHandler = (email, password) => {
+    //need to check email and password
     setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
   };
 
   return (
     <Fragment>
-      <Login onLogin={logginHandler} />
+      <MainHeader isAuthenticated={isLoggedIn} isLogout={logoutHandler} />
+      <main>
+        <Login onLogin={logginHandler} />
+      </main>
     </Fragment>
   );
 };
---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,11 @@
+import classes from "./MainHeader.module.css";
+
+const MainHeader = (props) => {
+  return (
+    <header className={classes["main-header"]}>
+      <h1>A Typical Page</h1>
+    </header>
+  );
+};
+
+export default MainHeader;
