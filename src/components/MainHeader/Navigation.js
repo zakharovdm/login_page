5b3019e2f10b4aaf24ee9ba2c61d8eff2ccd4834
Dmitriy Zakharov <dzaharov794@gmail.com>
2022-10-19 22:17:05 +0300

    refactor: add useContext Hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,34 +1,32 @@
+import { useContext } from "react";
+
 import classes from "./Navigation.module.css";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <Button onClick={props.isLogout}>Logout</Button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <Button onClick={props.isLogout}>Logout</Button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
